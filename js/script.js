a8e6b40872a076578c4b7821b7cda20398b89181
MaxTF141 <isaacsmaxwill22@gmail.com>
2022-11-26 11:51:04 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,70 +1,67 @@
 //assigning variables to query Selectors
 const numberButtons = document.querySelectorAll('.button');
 const display = document.querySelector('#output');
+let emptyArray = [];
 
 //loop for the buttons so all buttons can be selected
 for(let b = 0; b < numberButtons.length; b++){
-    numberButtons[b].addEventListener('click',()=>{
-        // console.log(numberButtons[b].innerText)
-        let button = numberButtons[b].innerText;
-        display.value += button;
-    })
-}
-let decimalBtn = document.querySelector('.decimal');
-decimalBtn.addEventListener('click', (e)=>{
-    display.value += decimalBtn.innerText
-})
+    numberButtons[b].addEventListener('click',(e)=>{
+        switch(e.target.innerText){
+            case '.':
+                if(!emptyArray.includes('.') ){ 
+                    display.value += e.target.innerText;
+                    emptyArray.push(e.target.innerText);
+                    console.log(emptyArray);
+                }
+                break;
+                case '-':
+                case '+':
+                case '/':
+                case '*':
+                    if(!display.value.endsWith(e.target.innerText)){
+                        display.value += e.target.innerText;
+                        emptyArray = [];
+                        console.log(emptyArray);
+                    }
+                break;
+                default:                
+                display.value += e.target.innerText;
+                emptyArray.push(e.target.innerText)
+                console.log(e.target.innerText)
+                console.log(emptyArray)
+            }
+        })
+    }
 
-//Equation and display on the input element
 let equal = document.querySelector('#equalBtn');
-equal.addEventListener('click', ()=> {
-        try{
-            display.value = eval(display.value);
+equal.addEventListener('click', e => {
+    switch(e.target.innerText){
+        default:
+            console.log(display.value);
+            try{
+                if(display.value == ''){
+                    display.value = ''
+                }else{
+                display.value = eval(display.value).toFixed(2)
+                console.log(typeof(display.value))
+                emptyArray = [];
+                emptyArray = [...display.value.split('')]
+                console.log(emptyArray);
+        }
         }catch{
             display.value = 'Error!';
         }
-    
+    }
 })
 
 //delete button
 let del = document.querySelector('#delBtn');
 del.addEventListener('click', ()=>{
-    display.value = "";
+    display.value = display.value.slice(0, -1);
+    emptyArray.pop();
 })
-
-//decimal
-
-// if (/\b./.test(numberButtons)){
-    //     numberButtons += display;
-    // }
-    
-    
-    // function idk(){
-
-    // const decBtn = document.querySelector('#decimal');
-    // const disabledButton = ()=>{
-    //     decBtn.disabled = true
-    // }
-    // decBtn.addEventListener('click', ()=>{
-    //     if (display.innerHTML == decNone){
-    //         decBtn.disabled = true    
-    //     }
-        
-    // })      
-    // }
-    
 display.addEventListener('input',function(e) {
     let decNone = /\.$/;
     let currentValue = e.target.value;
     console.log(decNone.test(currentValue));
 });
-
-
-
-// btn = /\b./;
-
-// button(includes(/\b"."/))
-
-// if (btn == true) {
-//     numberButtons += display;
-// } 
